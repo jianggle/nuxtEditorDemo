0b452441299624817544671cda1cfb21646be4c8
jianggle <599580031@qq.com>
2020-08-05 10:09:31 +0800

    去除eslint、nuxt等package升级

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,59 +2,46 @@ module.exports = {
 	head: {
 		title: 'nuxt-quill-demo',
 		meta: [
-            {charset: 'utf-8'},
-            {name: 'renderer',content: 'webkit'},
-            {'http-equiv': 'X-UA-Compatible',content: 'IE=edge,chrome=1'},
-            {hid: 'keywords',name: 'keywords',content: 'vue,nuxt,quill,editor'},
-			{hid: 'description',name: 'description',content: 'a nuxt-quill-demo'}
+			{ charset: 'utf-8' },
+			{ hid: 'keywords', name: 'keywords', content: 'vue,nuxt,quill,editor' },
+			{ hid: 'description', name: 'description', content: 'a nuxt-quill-demo' },
+			{ name: 'renderer', content: 'webkit' },
+			{ 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' },
 		],
 	},
 	loading: {
 		color: '#3468A3'
-    },
+	},
 	build: {
-		extend(config, {isDev,isClient}) {
-			if (isDev && isClient) {
-				config.module.rules.push({
-					enforce: 'pre',
-					test: /\.(js|vue)$/,
-					loader: 'eslint-loader',
-					exclude: /(node_modules)/
-				})
-            }
-            if(isClient){
-                config.entry['polyfill'] = ['babel-polyfill']
-            }
+		babel: {
+			"plugins": [
+				[
+					"component",
+					{
+						"libraryName": "element-ui",
+						"styleLibraryName": "theme-chalk"
+					}
+				]
+			],
+			"comments": false
 		},
-		vendor: [
-			'axios',
-            '~/plugins/element-ui',
-            '~/plugins/nuxt-quill-plugin.js'
-        ],
-        babel: {
-            "plugins": [
-                [
-                    "component",
-                    {
-                        "libraryName": "element-ui",
-                        "styleLibraryName": "theme-chalk"
-                    }
-                ]
-            ],
-            "comments": false
-        },
-        extractCSS: {
-            allChunks: true
-        }
+		extractCSS: process.env.NODE_ENV === 'production',
 	},
 	plugins: [
-        { src: '~/plugins/element-ui', ssr: true },
-        { src: '~/plugins/nuxt-quill-plugin.js', ssr: false }
-    ],
+		'~/plugins/element-ui',
+		{ src: '~/plugins/nuxt-quill-plugin.js', mode: 'client' }
+	],
+	render: {
+		resourceHints: false
+	},
 	css: [
-        {src: '~assets/scss/reset.scss',lang: 'scss'},
-        'quill/dist/quill.snow.css',
-        'quill/dist/quill.bubble.css',
-        'quill/dist/quill.core.css'
-    ]
+		{ src: '~assets/scss/reset.scss', lang: 'scss' },
+		'quill/dist/quill.snow.css',
+		'quill/dist/quill.bubble.css',
+		'quill/dist/quill.core.css'
+	],
+	server: {
+		port: '8887',
+		host: '0.0.0.0'
+	},
 }
